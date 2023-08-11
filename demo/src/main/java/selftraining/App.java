84153e862b5978998b0df1d1857f90ba08204132
ramagirisunny <ramagirisunny11@gmail.com>
2023-08-11 23:14:28 +0100

    other project

---
+++ demo/src/main/java/selftraining/App.java
@@ -0,0 +1,13 @@
+package selftraining;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
